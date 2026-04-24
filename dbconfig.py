f118e4eb320b63679c172acc05f3b0528707f530
Gerry Callaghan <gerrycallaghan2@gmail.com>
2026-04-24 15:57:17 +0100

    changed config details

--- dbconfig.py
+++ dbconfig.py
@@ -1,5 +1,5 @@
 config_details = {
-    "host" :"callagg2.mysql.pythonanywhere-services.com",
+    "host" :"callagg2.mysql.pythonanywhere-services.comt",
     "user" :"callagg2",
     "password" :"wsaaproject",
     "database" :"callagg2$wsaa"
