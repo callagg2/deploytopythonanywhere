fcf85c804255bf99bd561245f2b796d27be478d9
Gerry Callaghan <gerrycallaghan2@gmail.com>
2026-04-28 10:06:11 +0100

    added a show all button and a search button

--- staticpages/api.js
+++ staticpages/api.js
@@ -1,11 +1,54 @@
-const uri = "http://127.0.0.1:5000/books";
+const url = "/books";
 let books = [];
 
 function getBookItems() {
-  fetch(uri)
+  fetch(url)
     .then(response => response.json())
     .then(data => _displayItems(data))
     .catch(error => console.error("Unable to get books.", error));
+}
+
+function getOneBookItem() {
+  const id = document.getElementById("search-id").value.trim();
+
+  fetch(`${url}/${id}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Book not found");
+      }
+      return response.json();
+    })
+    .then(data => {
+      _displayItems([data]); // show the book found
+       $('#searchBookModal').modal('hide');   // auto-close the modal
+    })
+    .catch(error => console.error("Unable to find that book.", error));
+
+  return false; // prevent form reload
+}
+
+
+function findBookForm(id) {
+  const item = books.find(item => item.id === id);
+  document.getElementById("find-id").value = item.id;
+}
+
+$('#searchBookModal').on('show.bs.modal', function () {
+  document.getElementById("search-id").value = "";
+});
+
+
+function _displayfindbook(data) {
+
+  data.forEach(item => {
+    let searchButton = document.createElement("a"); // create a button element to be used for the search button in the table
+    searchButton.href = "#findBookModal"; // set the href attribute of the search button to the id of the modal that will be used to display the search results
+    searchButton.className = "search"; // set the class name of the search button to "search" for styling purposes
+    searchButton.setAttribute("onclick", `findBookForm(${item.id})`); // set the onclick attribute of the search button to call the findBookForm function with the id of the book item as an argument when the button is clicked
+    searchButton.setAttribute("data-toggle", "modal"); // set the data-toggle attribute of the search button to "modal" to enable the modal functionality
+    searchButton.innerHTML =
+      "<i class='material-icons' data-toggle='tooltip' title='Search'>&#xE8B6;</i>";
+  });
 }
 
 function addBookItem() {
@@ -15,6 +58,14 @@ function addBookItem() {
   const publisherInputText = document.getElementById("add-publisher");
   const priceInputText = document.getElementById("add-price");
 
+$('#addBookModal').on('show.bs.modal', function () {
+  document.getElementById("add-ID").value = "";
+  document.getElementById("add-title").value = "";
+  document.getElementById("add-author").value = "";
+  document.getElementById("add-publisher").value = "";
+  document.getElementById("add-price").value = "";
+});
+
   const item = {
     id: parseInt(idInputText.value.trim()),
     title: titleInputText.value.trim(),
@@ -23,7 +74,7 @@ function addBookItem() {
     price: parseInt(priceInputText.value.trim())
   };
   console.log(JSON.stringify(item));
-  fetch(uri, {
+  fetch(url, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -45,7 +96,7 @@ function addBookItem() {
 
 function deleteBookItem() {
   const itemId = document.getElementById("delete-id").value.trim();
-  fetch(`${uri}/${itemId}`, {
+  fetch(`${url}/${itemId}`, {
     method: "DELETE"
   })
     .then(() => getBookItems())
@@ -77,7 +128,7 @@ function updateBookItem() {
     price: parseInt(document.getElementById("edit-price").value.trim())
   };
 
-  fetch(`${uri}/${itemId}`, {
+  fetch(`${url}/${itemId}`, {
     method: "PUT",
     headers: {
       Accept: "application/json",
@@ -99,10 +150,10 @@ function _displayCount(itemCount) {
 }
 
 function _displayItems(data) {
-  const tBody = document.getElementById("books");
+  const tBody = document.getElementById("books"); // get the table body element by its id
   tBody.innerHTML = "";
-  _displayCount(data.length);
-  const button = document.createElement("button");
+  _displayCount(data.length); // display the number of book items in the table
+  const button = document.createElement("button"); // create a button element to be used for the edit and delete buttons in the table
 
   data.forEach(item => {
     let editButton = document.createElement("a");
@@ -121,27 +172,28 @@ function _displayItems(data) {
     deleteButton.innerHTML =
       "<i class='material-icons' data-toggle='tooltip' title='Delete'>&#xE872;</i>";
 
+    // each tr is a row in the table, so we insert a new row for each book item    
     let tr = tBody.insertRow();
-
+    // each td is a column in the table, so we insert 6 columns for each book item
     let td1 = tr.insertCell(0);
     let textid = document.createTextNode(item.id);
-    td1.appendChild(textid);
+    td1.appendChild(textid); // add the book id to the first column of the table
 
     let td2 = tr.insertCell(1);
     let textTitle = document.createTextNode(item.title);
-    td2.appendChild(textTitle);
+    td2.appendChild(textTitle); // add the book title to the second column of the table
 
     let td3 = tr.insertCell(2);
     let textAuthor = document.createTextNode(item.author);
-    td3.appendChild(textAuthor);
+    td3.appendChild(textAuthor); // add the book author to the third column of the table
 
     let td4 = tr.insertCell(3);
     let textPublisher = document.createTextNode(item.publisher);
-    td4.appendChild(textPublisher);
+    td4.appendChild(textPublisher); // add the book publisher to the fourth column of the table
 
     let td5 = tr.insertCell(4);
     let textPrice = document.createTextNode(item.price);
-    td5.appendChild(textPrice);
+    td5.appendChild(textPrice); // add the book price to the fifth column of the table
 
     let td6 = tr.insertCell(5);
     td6.appendChild(editButton);
